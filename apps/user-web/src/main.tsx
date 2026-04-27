65a0d3134589869b933f8ae467722ae44c82c5a2
CongKE1940 <92537310+CongKE1940@users.noreply.github.com>
2026-04-27 11:31:59 +0800

    Load navigation redesign styles in user app

--- apps/user-web/src/main.tsx
+++ apps/user-web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import "@aios/ui-web/src/ui-shell.css";
 import "@aios/ui-web/src/aios-redesign.css";
+import "@aios/ui-web/src/navigation-redesign.css";
 
 import { UserApp } from "./app";
 
