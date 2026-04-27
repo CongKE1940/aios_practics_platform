48f24f18e543404f48e63ae538fb5fc38632cb33
CongKE1940 <92537310+CongKE1940@users.noreply.github.com>
2026-04-27 10:03:52 +0800

    Load AIOS redesign styles in user app

--- apps/user-web/src/main.tsx
+++ apps/user-web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "@aios/ui-web/src/ui-shell.css";
+import "@aios/ui-web/src/aios-redesign.css";
 
 import { UserApp } from "./app";
 
